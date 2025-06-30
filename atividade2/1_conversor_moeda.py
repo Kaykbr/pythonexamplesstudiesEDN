c9ca943817a16ca63c9bc0a9633501095645fc53
Kayky Bezerra Da Silva <kaykybritosilva23@gmail.com>
2025-06-29 23:42:34 -0300

    feat: adiciona exercícios intermediários da atividade 2

---
+++ atividade2/1_conversor_moeda.py
@@ -0,0 +1,12 @@
+valor_reais = 100.00
+taxa_dolar = 5.20
+taxa_euro = 6.15
+
+valor_dolares = valor_reais / taxa_dolar
+valor_euros = valor_reais / taxa_euro
+
+print(f"Valor em reais: R$ {valor_reais:.2f}")
+print(f"Taxa do dólar: R$ {taxa_dolar:.2f}")
+print(f"Taxa do euro: R$ {taxa_euro:.2f}")
+print(f"Valor em dólares: $ {valor_dolares:.2f}")
+print(f"Valor em euros: € {valor_euros:.2f}")
