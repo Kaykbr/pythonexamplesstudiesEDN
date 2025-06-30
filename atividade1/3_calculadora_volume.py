0d254d1193076aec0060ef88bf5d8a287ddbc1dc
Kayky Bezerra Da Silva <kaykybritosilva23@gmail.com>
2025-06-29 23:42:13 -0300

    feat: adiciona exercícios básicos da atividade 1

---
+++ atividade1/3_calculadora_volume.py
@@ -0,0 +1,8 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print(f"Volume da caixa: {volume} cm³")
+print(f"Dimensões: {comprimento}cm x {largura}cm x {altura}cm")
